1fdfc36357744312111506e5b7fe424df107ae4b
KLAUS WONG (SYS-TSV-ISD-OOCLL/HKG) <WONGKL2@OOCL.COM>
2021-11-30 19:39:43 +0800

    [PosMachine][Klaus] feat: add convertToItems function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -13,27 +13,27 @@ public class PosMachine {
     private List<ItemInfo> loadAllItemsInfo(){
         return ItemDataLoader.loadAllItemInfos();
     }
-//    private List<SingleItem> convertToItems(List<String> barcodes) {
-//        List<ItemInfo> itemDetailList = loadAllItemsInfo();
-//        List<SingleItem> itemList = new ArrayList<>();
-//        List<String> distinctBarcodes = barcodes.stream()
-//                .distinct()
-//                .collect(Collectors.toList());
-//        for (String barcode : distinctBarcodes) {
-//            for (ItemInfo itemInfo : itemDetailList) {
-//                if (barcode.equals(itemInfo.getBarcode())) {
-//                    int quantity = Collections.frequency(barcodes, barcode);
-//                    int unitPrice = itemInfo.getPrice();
-//                    int subTotal = calculateSubtotal(quantity, unitPrice);
-//                    SingleItem item = new SingleItem(itemInfo.getName(), quantity, unitPrice, subTotal);
-//                    itemList.add(item);
-//                }
-//            }
-//        }
-//        return itemList;
-//    }
-//
-//    private int calculateSubtotal(int quantity, int unitPrice) {
-//        return quantity * unitPrice;
-//    }
+    private List<SingleItem> convertToItems(List<String> barcodes) {
+        List<ItemInfo> itemDetailList = loadAllItemsInfo();
+        List<SingleItem> itemList = new ArrayList<>();
+        List<String> distinctBarcodes = barcodes.stream()
+                .distinct()
+                .collect(Collectors.toList());
+        for (String barcode : distinctBarcodes) {
+            for (ItemInfo itemInfo : itemDetailList) {
+                if (barcode.equals(itemInfo.getBarcode())) {
+                    int quantity = Collections.frequency(barcodes, barcode);
+                    int unitPrice = itemInfo.getPrice();
+                    int subTotal = calculateSubtotal(quantity, unitPrice);
+                    SingleItem item = new SingleItem(itemInfo.getName(), quantity, unitPrice, subTotal);
+                    itemList.add(item);
+                }
+            }
+        }
+        return itemList;
+    }
+
+    private int calculateSubtotal(int quantity, int unitPrice) {
+        return quantity * unitPrice;
+    }
 }
